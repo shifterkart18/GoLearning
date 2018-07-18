ccad51f6399468dcc83ff0095290d533fc74c96f
amartz <shifterkart18@gmail.com>
2018-07-18 09:00:34 -0500

    More playing around with Go

--- HelloWorld/Main.go
+++ HelloWorld/Main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"runtime"
 	"time"
 )
+
+const Name = "Andrew Martz"
+
+var int Number = 18
 
 func main() {
 
@@ -11,9 +17,29 @@ func main() {
 
 	fmt.Println("The time is currently: ", time.Now())
 
-	fmt.Printf("This is how a formatted string works:\nint: %v \nfloat: %v \nstring: %v\n", 18, 3.14, "Andrew\n")
+	fmt.Println("My name is: ", Name)
+
+	fmt.Println("A package variable: ", Number)
+	Number := 21
+	fmt.Println("A package variable with a new value: ", Number)
+
+	fmt.Printf("This is how a formatted string works:\nint: %v \nfloat: %v \nstring: %v\n", 18, math.Pi, "Andrew\n")
 
 	FizzBuzz(30)
 
 	Test321(-100, 100)
+}
+
+func PrintOS() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
 }
